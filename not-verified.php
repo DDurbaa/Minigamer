a434c35655fdc758d307926810415ba6ed93eb66
simyxx <simonecek2005@gmail.com>
2024-06-20 18:25:48 +0200

    styly pro verifikacni stranky

--- not-verified.php
+++ not-verified.php
@@ -9,15 +9,20 @@ if (isset($_SESSION['user_id']))
 {
     
     $DB = new DB();
-    $result = $DB->query("SELECT verified FROM users WHERE id = ? LIMIT 1", $_SESSION['user_id']);
+    $result = $DB->query("SELECT verified, email FROM users WHERE id = ? LIMIT 1", $_SESSION['user_id']);
     $user = $result->fetchArray();
 
-    if ($user['verified'] == 1) 
+    if ($user['verified'] == 2) 
     {
         header("Location: index.php");
         exit();
     }
+    $email = $user['email'];
+    $parts = explode('@', $email); // Rozdělíme e-mail na část před @ a za @
+    $masked_email = substr($parts[0], 0, 2) . str_repeat('*', strlen($parts[0]) - 2) . '@' . substr($parts[1], 0, 2) . str_repeat('*', strlen($parts[1]) - 2);
 
+    $colors = ['#CD090F', '#ffcc00', '#00ff00', '#007fff'];
+    $random_color = $colors[array_rand($colors)];
 }
 ?>
 <!DOCTYPE html>
@@ -26,12 +31,81 @@ if (isset($_SESSION['user_id']))
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="mlogo.png">
-    <title>Verify Your Account!</title>
+    <title>Minigamer | Verify Your Account</title>
+    <style>
+        body {
+            background-color: #1a1a1a;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            margin: 0;
+            font-family: Arial, sans-serif;
+        }
+
+        header {
+            position: absolute;
+            top: 20px;
+        }
+
+        .logo img {
+            height: auto;
+        }
+
+        .description {
+            text-align: center;
+        }
+
+        .description h1 {
+            color:
+                <?php echo $random_color; ?>
+            ;
+            font-size: 4em;
+            margin-bottom: 0.2em;
+        }
+
+        .description p {
+            color: white;
+            font-size: 3em;
+            margin-top: 0;
+            margin-bottom: 1em;
+        }
+
+        .description a {
+            color: white;
+            font-size: 2.5em;
+            text-decoration: none;
+            border: 2px solid
+                <?php echo $random_color; ?>
+            ;
+            padding: 10px 20px;
+            border-radius: 5px;
+            transition: background-color 0.3s, color 0.3s;
+        }
+
+        .description a:hover {
+            background-color:
+                <?php echo $random_color; ?>
+            ;
+            color: #1a1a1a;
+        }
+    </style>
 </head>
 <body>
 
-    <h1>Verify Your Account Before Proceeding!</h1>
-    <p>A verification e-mail has been sent to your e-mail address, verify your account to proceed.</p>
+<header>
+        <div class="logo">
+            <img src="mlogo.png" alt="Minigamer Logo">
+        </div>
+    </header>
+    <main>
+        <div class="description">
+        <h1>Verify Your Account To Proceed!</h1>
+        <p>A verification e-mail has been sent to <?php echo $masked_email ?>.</p>
+            <a href="index.php">RELOAD</a>
+        </div>
+    </main>
     
 </body>
 </html>
--- verify-email.php
+++ verify-email.php
@@ -1,45 +1,116 @@
-<?php 
+<?php
 
 include "inc/loader.php";
 $msg = "";
 
-if (isset($_GET['t'])) 
-{
+if (isset($_GET['t'])) {
     $token = $_GET['t'];
     $DB = new DB();
     $DB->query("SELECT * FROM users WHERE token = ?", $token);
-    
-    if ($DB->numRows() > 0) 
-    {
+
+    if ($DB->numRows() > 0) {
         $DB->query("UPDATE users SET verified = 1 WHERE token = ?", $token);
-        $msg =  "Email verified successfully!";
-    } 
-    else 
-    {
-        $msg = "Invalid token!";
+        $msg = "Email verified successfully!";
+    } else {
+        $msg = "Invalid e-mail token!";
     }
-} 
-else 
-{
+} else {
     header("Location: index.php");
 }
+
+$colors = ['#CD090F', '#ffcc00', '#00ff00', '#007fff'];
+$random_color = $colors[array_rand($colors)];
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="mlogo.png">
-    <title>Email Verification</title>
+    <title>Minigamer | Verify Your E-mail</title>
+    <style>
+        body {
+            background-color: #1a1a1a;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            margin: 0;
+            font-family: Arial, sans-serif;
+        }
+
+        header {
+            position: absolute;
+            top: 20px;
+        }
+
+        .logo img {
+            height: auto;
+        }
+
+        .description {
+            text-align: center;
+        }
+
+        .description h1 {
+            color:
+                <?php echo $random_color; ?>
+            ;
+            font-size: 14em;
+            margin-bottom: 0.2em;
+        }
+
+        .description p {
+            color: white;
+            font-size: 3em;
+            margin-top: 0;
+            margin-bottom: 1em;
+        }
+
+        .description a {
+            color: white;
+            font-size: 2.5em;
+            text-decoration: none;
+            border: 2px solid
+                <?php echo $random_color; ?>
+            ;
+            padding: 10px 20px;
+            border-radius: 5px;
+            transition: background-color 0.3s, color 0.3s;
+        }
+
+        .description a:hover {
+            background-color:
+                <?php echo $random_color; ?>
+            ;
+            color: #1a1a1a;
+        }
+    </style>
 </head>
+
 <body>
 
-    <?php 
-        if ($msg != "")
-        {
-            echo $msg;
-        }
-    ?>
-    
+    <header>
+        <div class="logo">
+            <img src="mlogo.png" alt="Minigamer Logo">
+        </div>
+    </header>
+    <main>
+        <div class="description">
+            <p>
+                <?php
+                if ($msg != "") {
+                    echo $msg;
+                }
+                ?>
+            </p>
+
+            <a href="index.php">GO BACK</a>
+        </div>
+    </main>
+
 </body>
+
 </html>
